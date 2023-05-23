a9cf97cc237018f347bd986831886ab754a4d8f4
Your Name <jonywol@outlook.com>
2023-05-23 11:42:17 +1000

    exciting stuff

--- main.js
+++ main.js
@@ -13,3 +13,35 @@ menuBtn.addEventListener("click", function() {
     isNavHidden = true;
   }
 });
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+var activePanel = null;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    if (activePanel && activePanel !== this) {
+      activePanel.classList.remove("active");
+      activePanel.nextElementSibling.style.maxHeight = null;
+    }
+    
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    
+    if (this.classList.contains("active")) {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+      activePanel = this;
+    } else {
+      panel.style.maxHeight = null;
+      activePanel = null;
+    }
+  });
+}
+
+
+
+
+
+
+
